f57dd8bfbf23f2924049b43e330b95e9f8ed1577
florencekarumanchi-dev <florencekarumanchi@gmail.com>
2026-07-24 10:54:10 +0530

    Add files via upload

---
+++ DeepSkilling/Module 03/CreateStoredProcedure/Queries/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employees (FirstName, LastName, DepartmentID, Salary, JoinDate)
+VALUES
+('Sai', 'Babu', 101, 50000, '2024-01-15'),
+('Rahul', 'Sharma', 102, 60000, '2023-06-20'),
+('Anjali', 'Reddy', 101, 55000, '2022-08-10'),
+('Priya', 'Kumar', 103, 65000, '2021-04-18');
